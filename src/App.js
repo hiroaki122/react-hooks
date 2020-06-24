d58a23928cf1c8b0ab1f2e3bfd73d56446f7c57a
hiroaki122 <jiro.h.v.122@gmail.com>
2020-06-24 16:21:29 +0900

    object-setState

--- src/App.js
+++ src/App.js
@@ -5,22 +5,27 @@ const App = () => {
     name: "",
     price: 1000,
   };
-  const [name, setName] = useState(initialStates.name);
-  const [price, setPrice] = useState(initialStates.price);
+  const [state, setState] = useState(initialStates);
 
   const reset = () => {
-    setPrice(initialStates.price);
-    setName(initialStates.name);
+    setState(initialStates);
   };
   return (
     <>
       <p>
-        現在の{name}は、{price}円です。
+        現在の{state.name}は、{state.price}円です。
       </p>
-      <button onClick={() => setPrice(price + 1)}>+1</button>
-      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={() => setState({ ...state, price: state.price + 1 })}>
+        +1
+      </button>
+      <button onClick={() => setState({ ...state, price: state.price - 1 })}>
+        -1
+      </button>
       <button onClick={reset}>reset</button>
-      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <input
+        value={state.name}
+        onChange={(e) => setState({ ...state, name: e.target.value })}
+      />
     </>
   );
 };
