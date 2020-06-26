5017155bda8d76c8295b78ac8b341c1a515c059b
hiroaki122 <jiro.h.v.122@gmail.com>
2020-06-26 14:58:36 +0900

    create operation logs reducer

---
+++ src/reducers/operationLogs.js
@@ -0,0 +1,18 @@
+import { ADD_ORERATION_LOG, DELETE_ALL_ORERATION_LOGS } from "../actions";
+
+const operationLogs = (state = [], action) => {
+  switch (action.type) {
+    case ADD_ORERATION_LOG:
+      const operationLog = {
+        description: action.despriction,
+        operatedAt: action.operatedAt,
+      };
+      return [operationLog, ...state];
+    case DELETE_ALL_ORERATION_LOGS:
+      return [];
+    default:
+      return state;
+  }
+};
+
+export default operationLogs;
