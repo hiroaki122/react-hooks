c84e903f129860b9a06213ded54d8a55c6dde01c
hiroaki122 <jiro.h.v.122@gmail.com>
2020-06-26 12:28:45 +0900

    solve-prop-drlling-issue

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,17 +1,22 @@
-import React, { useState } from "react";
-import { CREATE_EVENT, DELETE_ALL_EVENTS } from "../actions";
+import React, { useContext, useState } from "react";
 
-const EventForm = ({ state, dispatch }) => {
+import { CREATE_EVENT, DELETE_ALL_EVENTS } from "../actions";
+import AppContext from "../contexts/AppContext";
+
+const EventForm = () => {
+  const { state, dispatch } = useContext(AppContext);
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
 
   const addEvent = (e) => {
     e.preventDefault();
+
     dispatch({
       type: CREATE_EVENT,
       title,
       body,
     });
+
     setTitle("");
     setBody("");
   };
@@ -19,34 +24,35 @@ const EventForm = ({ state, dispatch }) => {
   const deleteAllEvents = (e) => {
     e.preventDefault();
     const result = window.confirm(
-      "全てのイベントを本当に削除してもいいですか？"
+      "全てのイベントを本当に削除しても良いですか？"
     );
     if (result) dispatch({ type: DELETE_ALL_EVENTS });
   };
 
   const unCreatable = title === "" || body === "";
+
   return (
     <>
       <h4>イベント作成フォーム</h4>
       <form>
-        <div className="form-goup">
+        <div className="form-group">
           <label htmlFor="formEventTitle">タイトル</label>
           <input
             className="form-control"
             id="formEventTitle"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
-          ></input>
+          />
         </div>
 
-        <div className="form-goup">
+        <div className="form-group">
           <label htmlFor="formEventBody">ボディー</label>
           <textarea
             className="form-control"
             id="formEventBody"
             value={body}
             onChange={(e) => setBody(e.target.value)}
-          ></textarea>
+          />
         </div>
 
         <button
